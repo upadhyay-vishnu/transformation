5fd593589a3c4616423cd39bfc3433cec52db83b
Ashish Ghosh <ashish@BCTC-J604P5WJ2D.local>
2025-08-28 10:12:28 +0530

    Fixed datetime to date issue

--- ebp_transformation/fidility/contribution_plan_level_report.py
+++ ebp_transformation/fidility/contribution_plan_level_report.py
@@ -13,4 +13,10 @@ class ContributionPlanLevelReport(BaseTransformer):
 
         df.reset_index(drop=True, inplace=True)
         df = df[~df.apply(lambda row: list(row) == header, axis=1)]
+        df.columns = df.columns.str.strip()
+
+        # Format Trade Date column to remove time component
+        if "Trade Date" in df.columns:
+            df['Trade Date'] = pd.to_datetime(df['Trade Date'], errors='coerce').dt.strftime("%Y-%m-%d")
+
         df.to_excel(self.output_path, index=False)
